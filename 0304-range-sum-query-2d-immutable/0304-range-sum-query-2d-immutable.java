50e2c3eb341475d859370cb5b66a0cb208437f1f
TamannaChoudhary-hub <psstamanna@gmail.com>
2026-07-13 13:34:17 +0530

    Time: 101 ms (83.73%), Space: 142.1 MB (44.74%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -14,27 +14,27 @@
 //     }
 // }
 
-class NumMatrix {
-    int[][] matrix;
-    public NumMatrix(int[][] matrix) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=1;j<matrix[0].length; j++){
-                matrix[i][j] += matrix[i][j-1];
-            }
-        }
-        for(int i=1;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length; j++){
-                matrix[i][j] += matrix[i-1][j];
-            }
-        }
-        this.matrix = matrix;
-    }
 
+class NumMatrix {
+    int[][] prefix;
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        prefix = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefix[i][j] =
+                        matrix[i - 1][j - 1]
+                      + prefix[i - 1][j]
+                      + prefix[i][j - 1]
+                      - prefix[i - 1][j - 1];
+            }
+        }
+    }
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int total = matrix[row2][col2];
-        int extra = (col1 != 0?matrix[row2][col1-1]:0) +
-                    (row1 != 0?matrix[row1-1][col2]:0) -
-                    ((row1 != 0 && col1 != 0)?matrix[row1-1][col1-1]:0);
-        return total - extra;
+        return prefix[row2 + 1][col2 + 1]
+             - prefix[row1][col2 + 1]
+             - prefix[row2 + 1][col1]
+             + prefix[row1][col1];
     }
 }
