e91e72af48b31d1d582a4938b96ef6a655c6ae64
TamannaChoudhary-hub <psstamanna@gmail.com>
2026-09-05 14:49:18 +0530

    Time: 3 ms (92.97%), Space: 46.3 MB (94.62%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -13,10 +13,8 @@ class Solution {
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (used[i])
-                continue;
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
-                continue;
+            if (used[i])continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])continue;
             used[i] = true;
             ds.add(nums[i]);
             solve(nums, used, ds, ans);
